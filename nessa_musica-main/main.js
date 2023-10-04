67076a51aed9577b7aecc10928165dfbed97d247
vanessadasilva96 <128062360+vanessadasilva96@users.noreply.github.com>
2023-10-04 11:31:45 -0300

    Add files via upload

---
+++ nessa_musica-main/main.js
@@ -0,0 +1,31 @@
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
